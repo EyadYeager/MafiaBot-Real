442a979b3a8584a7dc2aaed6edd9c16636ac28ff
Eyad Hammad <eyadgoku@gmail.com>
2023-11-27 11:45:26 +0100

    Modified commit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord import app_commands
 from discord.ui import Button
 from dotenv import load_dotenv
+import asyncio
 import os
 
 intents = discord.Intents.default()
@@ -12,9 +13,55 @@ NOT_STARTED = 0
 NIGHT = 1
 DAY = 2
 
-Mafia = "Mafia"
-Citizen = "Citizen"
-Doctor = "Doctor"
+Mafia = "mafia"
+Citizen = "citizen"
+Dead = "dead"
+Doctor = "doctor"
+power_roles = ['mafia', 'doctor']
+
+
+class Player:
+    def __init__(self, id, server):
+        self.id = id
+        self.alive = 1
+        self.role = None
+        self.vote = None
+        self.server = server
+        self.ingame = 1  # changes to 0 upon m!leave, will be removed from server.players upon game end
+        self.options = []  # nighttime options for power role
+        self.action = 0  # if a power role, if has performed night action or not
+        self.cur_choice = None  # if a power role, their choice for the night
+        self.lst_choice = None  # if parity cop, last choice
+
+
+async def death(channel, player, server):
+    server.players[player].alive = 0
+
+
+async def game_end(channel, winner, server):  # end of game message (role reveal, congratulation of winners)
+    server.running = 0
+    await channel.send('\n'.join([end_text[winner]] + ['The roles were as follows:'] + ['<@%s> : `%s`' % (player.id, player.role) for player in server.players.values()]))
+    for key in server.players:
+        if not server.players[key].ingame:
+            server.players.pop(key)
+
+
+async def check_end(channel, server):
+    if not sum([player.role == 'mafia' for player in server.players.values() if player.alive]):  # no mafia remaining
+        await game_end(channel, 'Town', server)
+        return 1
+    elif sum([player.role == 'mafia' for player in server.players.values() if player.alive]) >= sum(
+            [player.role != 'mafia' for player in server.players.values() if player.alive]):
+        await game_end(channel, 'Mafia', server)
+        return 1
+    return 0
+
+
+async def check_votes(channel, server):
+    for player in server.players.values():
+        if player.alive and player.vote == None:
+            return 0
+    return 1
 
 
 class MafiaGame:
@@ -23,6 +70,27 @@ class MafiaGame:
         self.players = []  # List of Player objects
         self.state = NOT_STARTED
         self.day_number = 0
+
+        class Server:
+            def __init__(self):
+                self.players = {}  # dictionary mapping player IDs to a Player class
+                self.running = 0
+                self.phase = 0  # 0 for night, 1 for day
+                self.actions = 0  # how many actions remain during nighttime
+                self.time = 0  # how much time remains in the phase
+                self.round = 0  # what day/night of the game it is (e.g. day 1, night 2, etc)
+                self.saves = []  # list of doctor saves (by ID)
+                self.settings = {
+                    'selfsave': 0,  # doctor can save themselves
+                    'conssave': 0,  # doctor can save the same person in consecutive turns
+                    'continue': 0,  # continue playing even if a player leaves
+                    'reveal': 0,  # reveal role of player upon death
+                }
+                self.setup = {
+                    'Citizen': 0,
+                    'doctor': 0,
+                    'mafia': 0
+                }
 
     async def start_game(self):
         # Start the game loop
@@ -48,7 +116,9 @@ class MafiaGame:
         self.state = NIGHT
         self.day_number += 1
         # Send night start message
-        await self.guild.send(f"Night {self.day_number} has begun!")
+        channel = client.get_channel(1178635785612824626)
+
+        await channel.send(f"Night {self.day_number} has begun!")
 
         # Allow Mafia to perform actions
         await self.mafia_night_actions()
@@ -60,7 +130,9 @@ class MafiaGame:
     async def start_day(self):
         self.state = DAY
         # Send day start message
-        await self.guild.send(f"Day {self.day_number} has begun!")
+        channel = client.get_channel(1178635785612824626)
+
+        await channel.send(f"Day {self.day_number} has begun!")
 
         # Allow players to discuss and vote
         await self.daytime_voting()
@@ -68,13 +140,6 @@ class MafiaGame:
     async def daytime_voting(self):
         MAD = 1
         # Implement logic for player discussion and voting
-
-
-class Player:
-    def __init__(self, member, role):
-        self.member = member
-        self.role = role
-        self.alive = True
 
 
 class Aclient(discord.Client):
@@ -111,15 +176,12 @@ async def embed(interaction: discord.Interaction):
 
 @tree.command(guild=discord.Object(id=1174666167227531345), name='start',
               description='start the game')
-async def game_start(interaction: discord.Interaction):
-    game = get_game(interaction.guild)
-    if game and game_state == NOT_STARTED:
-        if interaction.user not in [player.member for player in game.players]:
-            await interaction.send_message("You have joined the game :>")
-            game.players.append(Player(interaction.user, None))
-        else:
-            await interaction.send_message("You are already in the game :<(((")
+async def start_game(interaction: discord.Interaction):
+    guild = discord.Object(id=1174666167227531345)
 
+    mafia_instance = MafiaGame(interaction)
+    await mafia_instance.start_game()
+    # await game_instance.start_game()
 
 
 @tree.command(guild=discord.Object(id=1174666167227531345), name='join', description='Enters a voice channel')
@@ -167,5 +229,4 @@ games = []
 
 load_dotenv()
 discordToken = os.getenv('DISCORD')
-# plz work
 client.run(discordToken)
